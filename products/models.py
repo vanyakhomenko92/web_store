11d0ea752386514d7a70af0e3c97bfe3b18fef50
vanyakhomenko92 <iamminer2017@gmail.com>
2020-12-29 15:56:03 +0200

    5
    added products to admin panel

--- products/models.py
+++ products/models.py
@@ -14,8 +14,8 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=48, blank=True, null=True, default=None )
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    discount = models.IntegerField(default=0)
-    category = models.ForeignKey(ProductCategory,on_delete=models.CASCADE, blank=True, null=True, default=None)
+    discount = models.IntegerField(default=0) #скидка
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, blank=True, null=True, default=None)
     short_description = models.TextField(blank=True, null=True, default=None)
     description = models.TextField(blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
