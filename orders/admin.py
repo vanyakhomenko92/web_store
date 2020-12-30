3d05ac26c69ec23db3bd06c96ece28b95893a9df
vanyakhomenko92 <iamminer2017@gmail.com>
2020-12-30 18:54:15 +0200

    10
    added basket_function

--- orders/admin.py
+++ orders/admin.py
@@ -1,28 +1,44 @@
 from django.contrib import admin
 from .models import *
 
+
 class ProductInOrderInline(admin.TabularInline):
     model = ProductInOrder
     extra = 0
 
-class StatusAmin(admin.ModelAdmin):
-    list_display =  [field.name for field in Status._meta.fields]
+
+class StatusAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in Status._meta.fields]
+
     class Meta:
         model = Status
 
-admin.site.register(Status, StatusAmin)
+admin.site.register(Status, StatusAdmin)
 
-class OrderAmin(admin.ModelAdmin):
-    list_display =  [field.name for field in Order._meta.fields]
+
+class OrderAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.fields]
     inlines = [ProductInOrderInline]
+
     class Meta:
         model = Order
 
-admin.site.register(Order, OrderAmin)
+admin.site.register(Order, OrderAdmin)
 
-class ProductInOrderAmin(admin.ModelAdmin):
-    list_display =  [field.name for field in ProductInOrder._meta.fields]
+
+class ProductInOrderAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in ProductInOrder._meta.fields]
+
     class Meta:
         model = ProductInOrder
 
-admin.site.register(ProductInOrder, ProductInOrderAmin)
+admin.site.register(ProductInOrder, ProductInOrderAdmin)
+
+
+class ProductInBasketAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in ProductInBasket._meta.fields]
+
+    class Meta:
+        model = ProductInBasket
+
+admin.site.register(ProductInBasket, ProductInBasketAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,61 +1,62 @@
 from django.db import models
+from products.models import Product
 from django.db.models.signals import post_save
 
-from products.models import Product
-
-"""Працюємо з БД django"""
 
 class Status(models.Model):
-
-    name = models.CharField(max_length=24, blank=True, null=True, default=None )
+    name = models.CharField(max_length=24, blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=False)
-
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return 'Статус %s' % self.name
+        return "Статус %s" % self.name
 
     class Meta:
-        verbose_name = 'Статус'
-        verbose_name_plural = 'Статус замовлень'
+        verbose_name = 'Статус заказа'
+        verbose_name_plural = 'Статусы заказа'
 
 
 class Order(models.Model):
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0) #total price for all products in order
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#total price for all products in order
+    customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
     customer_email = models.EmailField(blank=True, null=True, default=None)
-    customer_name = models.CharField(max_length=128, blank=True, null=True, default=None )
     customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
     customer_address = models.CharField(max_length=128, blank=True, null=True, default=None)
-    comments = models.TextField(blank=True, default=None)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    comments = models.TextField(blank=True, null=True, default=None)
+    status = models.ForeignKey(Status,on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=False)
-
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "Замовлення %s %s" % (self.id, self.status.name)
+        return "Заказ %s %s" % (self.id, self.status.name)
 
     class Meta:
-        verbose_name = 'Замовник'
-        verbose_name_plural = 'Замовлення'
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def save(self, *args, **kwargs):
+
+        super(Order, self).save(*args, **kwargs)
+
 
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, blank=True, null=True, default=None)
     nmb = models.IntegerField(default=1)
     price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)# price_per_item * nmb
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#price*nmb
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return 'товар %s' % self.product.name
+        return "%s" % self.product.name
 
     class Meta:
-        verbose_name = 'Товар в замовленні'
-        verbose_name_plural = 'Товари в замовленні'
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
 
     def save(self, *args, **kwargs):
         price_per_item = self.product.price
@@ -63,6 +64,8 @@ class ProductInOrder(models.Model):
         self.total_price = self.nmb * price_per_item
 
         super(ProductInOrder, self).save(*args, **kwargs)
+
+
 
 def product_in_order_post_save(sender, instance, created, **kwargs):
     order = instance.order
@@ -75,4 +78,33 @@ def product_in_order_post_save(sender, instance, created, **kwargs):
     instance.order.total_price = order_total_price
     instance.order.save(force_update=True)
 
+
 post_save.connect(product_in_order_post_save, sender=ProductInOrder)
+
+
+
+class ProductInBasket(models.Model):
+    session_key = models.CharField(max_length=128, blank=True, null=True, default=None)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, blank=True, null=True, default=None)
+    nmb = models.IntegerField(default=1)
+    price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#price*nmb
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.product.name
+
+    class Meta:
+        verbose_name = 'Товар в корзине'
+        verbose_name_plural = 'Товары в корзине'
+
+
+    def save(self, *args, **kwargs):
+        price_per_item = self.product.price
+        self.price_per_item = price_per_item
+        self.total_price = int(self.nmb) * price_per_item
+
+        super(ProductInBasket, self).save(*args, **kwargs)
